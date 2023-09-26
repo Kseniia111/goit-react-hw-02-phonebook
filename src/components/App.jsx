dd076de31521bdf99b6042b03c8425df4bb6691d
Kseniia111 <kseniya.markovych@gmail.com>
2023-09-26 22:19:31 +0300

    no more red

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { nanoid } from 'nanoid';
 import { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
